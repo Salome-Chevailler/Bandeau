80648b3f272bc10e8d11e25badef2baf24942828
lisa <lisa.estebe@gmail.com>
2020-12-07 15:38:50 +0100

    DoigtDerriere

---
+++ src/main/java/exemple/DoigtDerriere.java
@@ -0,0 +1,30 @@
+package exemple;
+
+import bandeau.Bandeau;
+import java.awt.Font;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lisa
+ */
+public class DoigtDerriere extends EffetDAnimation {
+    
+    public DoigtDerriere(Bandeau monBandeau, int rep, String msg){
+        super(monBandeau, rep, msg);
+    }
+    
+    public void jouer(){
+        monBandeau.setMessage(msg);
+        for (int i=60; i>5; i-=5 ){
+            monBandeau.setFont ( new Font ("Dialog", Font.BOLD, 5-i));
+            monBandeau.sleep(100);
+        }
+    }
+    
+}
